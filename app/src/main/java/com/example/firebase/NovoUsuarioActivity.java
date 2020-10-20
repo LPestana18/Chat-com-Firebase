89ba596a822a0434722882c571353748006f65ab
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-10-19 21:49:40 -0300

    Chat com firebase

---
+++ app/src/main/java/com/example/firebase/NovoUsuarioActivity.java
@@ -0,0 +1,35 @@
+package com.example.firebase;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class NovoUsuarioActivity extends AppCompatActivity {
+
+    private EditText loginNovoUsuarioEditText;
+    private EditText senhaNovoUsarioEditText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_usuario);
+        loginNovoUsuarioEditText = findViewById(R.id.loginNovoUsuarioEditText);
+        senhaNovoUsarioEditText = findViewById(R.id.senhaNovoUsuarioEditText);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void criarNovoUsuario(View view) {
+        String login = loginNovoUsuarioEditText.getText().toString();
+        String senha  = senhaNovoUsarioEditText.getText().toString();
+        mAuth.createUserWithEmailAndPassword(login, senha).addOnSuccessListener((result)-> {
+            Toast.makeText(this, result.getUser().toString(), Toast.LENGTH_SHORT).show();
+            finish();
+        }).addOnFailureListener(error -> error.printStackTrace());
+    }
+}
